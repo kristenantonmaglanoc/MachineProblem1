6e9c4d1313d4f98c951c604d05925df99926edaa
kristenantonmaglanoc <52022026+kristenantonmaglanoc@users.noreply.github.com>
2019-07-14 00:42:31 +0800

    Update machineproblem1.cpp

--- machineproblem1.cpp
+++ machineproblem1.cpp
@@ -1,42 +1,42 @@
 #include <iostream>
-#include <conio.h>
-#include <cmath>
-using namespace std;
+#include <conio.h>  // to provide console input or output
+#include <cmath>  // declares a set of functions to compute mathematical operations
+using namespace std;  // pre processors
 
-int main() 
+int main()  // main body
 {
 	int c, b, ta, u, s, v, x;
-	cout << "Enter coefficients b and c: \n\n";
+	cout << "Enter coefficients b and c: \n\n";  // prompts for input
 	
 	cout << "b = ";
 	cin >> b;
 	cout << "c = ";
-	cin >> c;
+	cin >> c;  // values are input
 
 	ta = b + b - b; 
-	s = (b * b) - (4 * c);
-	u = ta + sqrt(s); 
+	s = (b * b) - (4 * c);  
+	u = ta + sqrt(s);   
 	u /= 2;
 	v = ta - sqrt(s);
-	v /= 2;
+	v /= 2;  // u and v formulas to be followed
 	
 	
 	if (s < 0)
-		cout << "u and v are complex numbers";
+		cout << "u and v are complex numbers";  // if less than 0, numbes are complex and therefore not factorable
 		
 	if (s > 0) {
 		cout << "\nYour integers are: \n\n" 
 		<< "u = " << u << endl;
 		cout << "v = " << v << endl;
-		cout << "\n(x + (" << u << "))(x + (" << v << "))";
+		cout << "\n(x + (" << u << "))(x + (" << v << "))";  // u and v are displayed as the integers of a polynomial
 		x = pow (s, 2);
-		x % s == 0;
+		x % s == 0;  // needs to be a square and divisible by 2, making it factorable
 		cout << "\nu and v are integers\n" << "\nThe polynomial is factorable";
 	}
 	else {
-		cout << "\nu and v are not integers\n" << "\nThe polynomial is not factorable";
+		cout << "\nu and v are not integers\n" << "\nThe polynomial is not factorable";  // non-factorable if it does not meet the formula
 	}
 		
 	getch();
-	return 0;		
+	return 0;  // end statements		  
 }
